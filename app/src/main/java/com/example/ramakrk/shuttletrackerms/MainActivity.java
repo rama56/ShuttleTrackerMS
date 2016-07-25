ae67823b8cc8ae407158de0c8b765aceab77ce0a
Vincent A <vinca@microsoft.com>
2016-07-25 20:05:11 +0530

    Basic UI added

--- app/src/main/java/com/example/ramakrk/shuttletrackerms/MainActivity.java
+++ app/src/main/java/com/example/ramakrk/shuttletrackerms/MainActivity.java
@@ -1,13 +1,65 @@
 package com.example.ramakrk.shuttletrackerms;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import android.view.View;
+import android.view.View.OnClickListener;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GoogleMap employeeMap;
+    private Button trackShuttle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        trackShuttle = (Button) findViewById(R.id.track);
+        SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                employeeMap = googleMap;
+
+                // Add a marker in Sydney, Australia, and move the camera.
+                LatLng sydney = new LatLng(17.385, 78.486);
+                LatLng sydney1 = new LatLng(17.386, 78.489);
+                employeeMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+                employeeMap.addMarker(new MarkerOptions().position(sydney1).title("You are here...."));
+                employeeMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney,15.0f));
+
+            }
+        });
+
+        trackShuttle.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                alertDialog.setTitle("Alert");
+                alertDialog.setMessage("Alert message to be shown");
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                alertDialog.show();
+
+            }
+        });
     }
+
+
 }
